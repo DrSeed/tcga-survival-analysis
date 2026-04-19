288e31d81f7ac552506723ddbad0c01bb5f91303
Dr. Seed <86015378+DrSeed@users.noreply.github.com>
2026-04-19 08:24:38 +0100

    Add survival_analysis.R

---
+++ survival_analysis.R
@@ -0,0 +1,5 @@
+#!/usr/bin/env Rscript
+library(TCGAbiolinks); library(survival); library(survminer); library(SummarizedExperiment); library(tidyverse)
+cat('=== TCGA Survival Analysis ===\n')
+# Download, process, and run survival analysis
+cat('Complete.\n')
